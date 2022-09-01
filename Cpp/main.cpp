af841e326f6ec4f9352dd09c9e60fee59d2d46a0
tuduan1902 <89310065+tuduan1902@users.noreply.github.com>
2022-09-01 17:47:56 +0700

    Update main.cpp

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,9 +1,59 @@
-#include<iostream>  
-#include "functions.h"  
-int main()  
-{  
-    print_hello();  
-    std::cout<< std::endl;  
-    std::cout<< "The factorial of 5 is " << factorial(5) << std:: endl;  
-    return 0;  
-}  
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct PHANSO {
+	int t, m;
+};
+
+void rutGon(PHANSO& x);
+PHANSO tinhTong(PHANSO a, PHANSO b);
+int UCLN(int x, int y);
+void nhapPS(PHANSO& x);
+void xuatPS(PHANSO& x);
+
+int main() {
+	PHANSO a, b;
+	nhapPS(a);
+	nhapPS(b);
+	PHANSO t = tinhTong(a, b);
+	xuatPS(t);
+	return 0;
+}
+
+int UCLN(int x, int y) {
+	x = abs(x);
+	y = abs(y);
+	if (x * y == 0)
+		return x + y;
+	while (x != y) {
+		if (x > y) {
+			x = x - y;
+		}
+		else {
+			y = y - x;
+		}
+	}
+	return x;
+}
+
+void rutGon(PHANSO& x) {
+	int uc = UCLN(x.t, x.m);
+	x.t = x.t / uc;
+	x.m = x.m / uc;
+}
+
+PHANSO tinhTong(PHANSO a, PHANSO b) {
+	PHANSO tong;
+	tong.t = a.t * b.m + a.m * b.t;
+	tong.m = a.m * b.m;
+	rutGon(tong);
+	return tong;
+}
+
+void nhapPS(PHANSO &x) {
+	cin >> x.t >> x.m;
+}
+
+void xuatPS(PHANSO& x) {
+	cout << x.t << "/" << x.m;
+}
